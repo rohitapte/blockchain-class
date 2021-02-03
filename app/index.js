aa482025a77c775b1d6896d8ebe8a819a83bb08c
Rohit Apte <rohit@rohitapte.com>
2021-02-03 18:11:41 +0800

    transactions pool

--- app/index.js
+++ app/index.js
@@ -2,16 +2,35 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 app.use(bodyParser.json());
 const bc = new Blockchain();
-const p2pServer = new P2pServer(bc);
+const tp = new TransactionPool();
+const wallet = new Wallet();
+const p2pServer = new P2pServer(bc, tp);
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
+});
+
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+});
+
+app.get('/public-key', (req, res) => {
+    res.json({ publicKey: wallet.publicKey });
+});
+
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    p2pServer.broadcastTransaction(transaction);
+    res.redirect('/transactions');
 });
 
 app.post('/mine', (req, res) => {
--- wallet/index.js
+++ wallet/index.js
@@ -1,3 +1,4 @@
+const Transaction = require('./transaction');
 const ChainUtil = require('../chain-util');
 const { INITIAL_BALANCE } = require('../config');
 
@@ -16,6 +17,21 @@ class Wallet {
 
     sign(dataHash) {
         return this.keyPair.sign(dataHash);
+    }
+
+    createTransaction(recipient, amount, transactionPool) {
+        if (amount > this.balance) {
+            console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
+            return;
+        }
+        let transaction = transactionPool.existingTransaction(this.publicKey);
+        if (transaction) {
+            transaction.update(this, recipient, amount);
+        } else {
+            transaction = Transaction.newTransaction(this, recipient, amount);
+            transactionPool.updateOrAddTransaction(transaction);
+        }
+        return transaction;
     }
 }
 
